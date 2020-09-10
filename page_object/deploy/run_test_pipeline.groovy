275eaa4fa8c10f54836400631d25d90a19b3ae68
nineliuxin <1252367043@qq.com>
2020-09-10 21:19:25 +0800

    add pipeline job

---
+++ page_object/deploy/run_test_pipeline.groovy
@@ -0,0 +1,32 @@
+pipeline {
+    agent {
+        node{
+            #构建在MyNewNode的slave节点上运行
+            label 'MyNewNode'
+            customWorkspace "pipelineWorkspace"
+        }
+    }
+    stages {
+        #流水线触发begin、running、finish
+        stage("同步源码"){
+            git([url:'https://github.com/nineliuxin/excise.git',branch:'${branch}'])
+        }
+        stage("运行测试代码"){
+            sh '''
+                . ~/bash_profile
+                cd page_object
+                export PRO_PATH=`pwd`
+                export os_type=`uname`
+                cd data
+                if [[ "${os_type}" == "Darwin" ]]; then
+                    sed -i "" "s/device_ip/${device_ip}/g" driver.yml
+                else
+                    sed -i "s/device_ip/${device_ip}/g" driver.yml
+                fi
+                cd $PRO_PATH/testcases
+                pytest -v test_login.py --alluredir ../allure_results
+                allure serve ../allure_results
+            '''
+        }
+    }
+}
